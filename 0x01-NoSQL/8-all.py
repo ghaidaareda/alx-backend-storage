7146d26dab388dea86176f6caf9c08114036d12e
ghaidaareda <dodo.sa7afy@gmail.com>
2024-01-28 23:46:58 +0200

    8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+
+
 """
  Python function that lists all documents in a collection
 """
+
+
 def list_all(mongo_collection):
-    docs =  mongo_collection.find()  # Find returns a cursor object
+    """
+    lists all documents in a collection
+    """
+    docs = mongo_collection.find()  # Find returns a cursor object
     for doc in docs:
         # Iterate over the cursor to get each document
         return doc
