afbc31d046c0c169757bf5b2c1e6b2c3004dc752
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-03 01:01:35 +0300

    exercise.py 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-start up
+start up 
 """
 import random
 from typing import Callable, Union
@@ -15,18 +15,16 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def store(self, data:Union[str, bytes, int, float])->str:
         """
         store method
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-
-    def get(self, key: str, fn: Callable = None) -> Union[bytes, None]:
+    def get(self, key:str, fn:Callable = None) -> Union[bytes, None]:
         """
-        get method
+        get method 
         """
         value = self._redis.get(key)
         if value is None:
@@ -34,28 +32,16 @@ class Cache:
         if fn is not None:
             value = fn(value)
         return value
-
-    def get_str(self, key: str) -> Union[str, None]:
+    def get_str(self, key:str)->Union[str, None]:
         """
         retrieves data from Redis and converts it to a string.
         """
-        return self.get(key, fn=str)
-
-    def get_str(self, key: str) -> Union[int, None]:
+        return   self.get(key, fn=str)
+        
+    def get_int(self, key:str)->Union[int, None]:
         """
         retrieves data from Redis and converts it to a int.
         """
-        return self.get(key, fn=int)
+        return   self.get(key, fn=int)
 
-
-cache = Cache()
-
-TEST_CASES = {
-    b"foo": None,
-    123: int,
-    "bar": lambda d: d.decode("utf-8")
-}
-
-for value, fn in TEST_CASES.items():
-    key = cache.store(value)
-    assert cache.get(key, fn=fn) == value
+    
