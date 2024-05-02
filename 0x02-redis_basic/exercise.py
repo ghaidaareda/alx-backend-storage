65c046d5a080445ab03a28c58c08b4b9331a9a65
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-03 00:22:52 +0300

    exercise.py 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 start up
 """
 import random
-from typing import Union
+from typing import Callable, Union
 import redis
 import uuid
 
@@ -23,3 +23,39 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[bytes, None]:
+        """
+        get method
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        retrieves data from Redis and converts it to a string.
+        """
+        return self.get(key, fn=str)
+
+    def get_str(self, key: str) -> Union[int, None]:
+        """
+        retrieves data from Redis and converts it to a int.
+        """
+        return self.get(key, fn=int)
+
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
