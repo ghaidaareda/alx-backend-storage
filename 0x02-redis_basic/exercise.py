092d3f72c5429aa6017b1485e638686989a0bd00
ghaidaareda <dodo.sa7afy@gmail.com>
2024-05-04 00:03:59 +0300

    exercise.py 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
 """
-start up 
+start up
 """
 import random
 from typing import Callable, Union
 import redis
 import uuid
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+    return wrapper
 
 
 class Cache:
@@ -15,16 +25,19 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-    def store(self, data:Union[str, bytes, int, float])->str:
+
+    @count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         store method
         """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    def get(self, key:str, fn:Callable = None) -> Union[bytes, None]:
+
+    def get(self, key: str, fn: Callable = None) -> Union[bytes, None]:
         """
-        get method 
+        get method
         """
         value = self._redis.get(key)
         if value is None:
@@ -32,16 +45,15 @@ class Cache:
         if fn is not None:
             value = fn(value)
         return value
-    def get_str(self, key:str)->Union[str, None]:
+
+    def get_str(self, key: str) -> Union[str, None]:
         """
         retrieves data from Redis and converts it to a string.
         """
-        return   self.get(key, fn=str)
-        
-    def get_int(self, key:str)->Union[int, None]:
+        return self.get(key, fn=str)
+
+    def get_int(self, key: str) -> Union[int, None]:
         """
         retrieves data from Redis and converts it to a int.
         """
-        return   self.get(key, fn=int)
-
-    
+        return self.get(key, fn=int)
